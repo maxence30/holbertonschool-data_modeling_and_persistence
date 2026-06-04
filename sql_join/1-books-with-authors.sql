857c8161456a142b2296d9449acfd075a92925c1
maxence30 <henous.maxence@outlook.fr>
2026-06-04 14:43:40 +0200

    Relation Ship SQL Part. 1

---
+++ sql_join/1-books-with-authors.sql
@@ -0,0 +1,6 @@
+SELECT books.title,
+       authors.name AS author_name
+FROM books
+INNER JOIN authors
+ON books.author_id = authors.id
+ORDER BY books.title ASC;
---
+++ sql_join/2-authors-with-books.sql
@@ -0,0 +1,6 @@
+SELECT authors.name AS author_name,
+       books.title
+FROM authors
+LEFT JOIN books
+ON authors.id = books.author_id
+ORDER BY author_name ASC, title ASC;
